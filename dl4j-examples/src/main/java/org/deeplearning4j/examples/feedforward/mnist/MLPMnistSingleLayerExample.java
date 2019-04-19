6c343fb75119735a88a538a2c7af249f6873551e
Coder <1176513130@qq.com>
2019-04-19 17:19:04 +0800

    添加了保存和加载模型

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistSingleLayerExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistSingleLayerExample.java
@@ -1,7 +1,7 @@
 package org.deeplearning4j.examples.feedforward.mnist;
 
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -9,32 +9,39 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
-/**A Simple Multi Layered Perceptron (MLP) applied to digit classification for
+
+/**
+ * A Simple Multi Layered Perceptron (MLP) applied to digit classification for
  * the MNIST Dataset (http://yann.lecun.com/exdb/mnist/).
- *
+ * <p>
  * This file builds one input layer and one hidden layer.
- *
+ * <p>
  * The input layer has input dimension of numRows*numColumns where these variables indicate the
  * number of vertical and horizontal pixels in the image. This layer uses a rectified linear unit
  * (relu) activation function. The weights for this layer are initialized by using Xavier initialization
  * (https://prateekvjoshi.com/2016/03/29/understanding-xavier-initialization-in-deep-neural-networks/)
  * to avoid having a steep learning curve. This layer will have 1000 output signals to the hidden layer.
- *
+ * <p>
  * The hidden layer has input dimensions of 1000. These are fed from the input layer. The weights
  * for this layer is also initialized using Xavier initialization. The activation function for this
  * layer is a softmax, which normalizes all the 10 outputs such that the normalized sums
  * add up to 1. The highest of these normalized values is picked as the predicted class.
- *
  */
 public class MLPMnistSingleLayerExample {
 
@@ -42,6 +49,12 @@ public class MLPMnistSingleLayerExample {
 
     public static void main(String[] args) throws Exception {
         //number of rows and columns in the input pictures
+        for (int i = 0; i < 10; i++) {
+            train();
+        }
+    }
+
+    private static void train() throws IOException {
         final int numRows = 28;
         final int numColumns = 28;
         int outputNum = 10; // number of output classes
@@ -49,45 +62,50 @@ public class MLPMnistSingleLayerExample {
         int rngSeed = 123; // random number seed for reproducibility
         int numEpochs = 15; // number of epochs to perform
 
-        //Get the DataSetIterators:
-        DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
-        DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
-
-
-        log.info("Build model....");
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+        File locationToSave = new File("D:/dev/dl4j-examples/model/mnist-model.zip");
+        MultiLayerNetwork model;
+        if (locationToSave.exists()) {
+            model = ModelSerializer.restoreMultiLayerNetwork(locationToSave);
+        } else {
+            log.info("Build model....");
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(rngSeed) //include a random seed for reproducibility
                 // use stochastic gradient descent as an optimization algorithm
-                .updater(new Nesterovs(0.006, 0.9))
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(new Nesterovs(0.001, 0.9))
                 .l2(1e-4)
                 .list()
                 .layer(new DenseLayer.Builder() //create the first, input layer with xavier initialization
-                        .nIn(numRows * numColumns)
-                        .nOut(1000)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .layer( new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD) //create hidden layer
-                        .nOut(outputNum)
-                        .activation(Activation.SOFTMAX)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
+                    .nIn(numRows * numColumns)
+                    .nOut(1000)
+                    .activation(Activation.RELU)
+                    .weightInit(WeightInit.XAVIER)
+                    .build())
+                .layer(new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD) //create hidden layer
+                    .nOut(outputNum)
+                    .activation(Activation.SOFTMAX)
+                    .weightInit(WeightInit.XAVIER)
+                    .build())
                 .build();
-
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+            model = new MultiLayerNetwork(conf);
+        }
         model.init();
         //print the score with every 1 iteration
-        model.setListeners(new ScoreIterationListener(1));
+        model.setListeners(new ScoreIterationListener(1000));
 
         log.info("Train model....");
-        for( int i=0; i<numEpochs; i++ ){
+        //Get the DataSetIterators:
+        DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
+        DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
+        for (int i = 0; i < numEpochs; i++) {
             model.fit(mnistTrain);
         }
 
+        ModelSerializer.writeModel(model, locationToSave, true);
 
         log.info("Evaluate model....");
         Evaluation eval = new Evaluation(outputNum); //create an evaluation object with 10 possible classes
-        while(mnistTest.hasNext()){
+        while (mnistTest.hasNext()) {
             DataSet next = mnistTest.next();
             INDArray output = model.output(next.getFeatures()); //get the networks prediction
             eval.eval(next.getLabels(), output); //check the prediction against the true class
@@ -95,7 +113,6 @@ public class MLPMnistSingleLayerExample {
 
         log.info(eval.stats());
         log.info("****************Example finished********************");
-
     }
 
 }
